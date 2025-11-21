530584701099555868c89c1470a2fd61e0033af5
isacom <isaac.martinez.programacion@gmail.com>
2025-11-21 13:37:44 +0100

    Commit 6

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,30 +3,54 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float MoveSpeed = 1;
+    
     private BoardManager m_Board;
     private Vector2Int m_CellPosition;
     private bool m_IsGameOver;
-    
+    private Animator m_Animator;
+    private bool m_IsMoving;
+    private Vector3 m_MoveTarget;
+
+    private void Awake()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+
     public void Init()
     {
         m_IsGameOver = false;
     }
+
     public void Spawn(BoardManager boardManager, Vector2Int cell)
     {
         m_Board = boardManager;
-        MoveTo(cell);
+        MoveTo(cell, true);
     }
+
     public void GameOver()
     {
         m_IsGameOver = true;
     }
-    public void MoveTo(Vector2Int cell)
+
+    public void MoveTo(Vector2Int cell, bool immediate)
     {
         m_CellPosition = cell;
-        transform.position = m_Board.CellToWorld(m_CellPosition);
+
+        if (immediate)
+        {
+            m_IsMoving = false;
+            transform.position = m_Board.CellToWorld(m_CellPosition);
+        }
+        else
+        {
+            m_IsMoving = true;
+            m_MoveTarget = m_Board.CellToWorld(m_CellPosition);
+        }
+      
+        m_Animator.SetBool("Moving", m_IsMoving);
     }
 
-  
     private void Update()
     {
         if (m_IsGameOver)
@@ -38,15 +62,16 @@ public class PlayerController : MonoBehaviour
 
             return;
         }
+
         Vector2Int newCellTarget = m_CellPosition;
         bool hasMoved = false;
 
-        if(Keyboard.current.upArrowKey.wasPressedThisFrame)
+        if (Keyboard.current.upArrowKey.wasPressedThisFrame)
         {
             newCellTarget.y += 1;
             hasMoved = true;
         }
-        else if(Keyboard.current.downArrowKey.wasPressedThisFrame)
+        else if (Keyboard.current.downArrowKey.wasPressedThisFrame)
         {
             newCellTarget.y -= 1;
             hasMoved = true;
@@ -62,27 +87,46 @@ public class PlayerController : MonoBehaviour
             hasMoved = true;
         }
 
-        if(hasMoved)
+        if (hasMoved)
         {
-            //check if the new position is passable, then move there if it is.
             BoardManager.CellData cellData = m_Board.GetCellData(newCellTarget);
 
-            if(cellData != null && cellData.Passable)
+            if (cellData != null && cellData.Passable)
             {
                 GameManager.Instance.TurnManager.Tick();
 
                 if (cellData.ContainedObject == null)
                 {
-                    MoveTo(newCellTarget);
+                    MoveTo(newCellTarget, true);
                 }
-                else if(cellData.ContainedObject.PlayerWantsToEnter())
+                else if (cellData.ContainedObject.PlayerWantsToEnter())
                 {
-                    MoveTo(newCellTarget);
-                    //Call PlayerEntered AFTER moving the player! Otherwise not in cell yet
+                    MoveTo(newCellTarget, true);
                     cellData.ContainedObject.PlayerEntered();
+                }
+                else
+                {
+                    m_Animator.SetTrigger("Attack");
                 }
             }
         }
-    }
 
+        if (m_IsMoving)
+        {
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                m_MoveTarget,
+                MoveSpeed * Time.deltaTime
+            );
+
+            if (transform.position == m_MoveTarget)
+            {
+                m_IsMoving = false;
+                m_Animator.SetBool("Moving", false);
+                var cellData = m_Board.GetCellData(m_CellPosition);
+                if (cellData.ContainedObject != null)
+                    cellData.ContainedObject.PlayerEntered();
+            }
+        }
+    }
 }
